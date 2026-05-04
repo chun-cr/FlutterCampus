9596cb6984ebb21645cd7fe7109b5e0fb5f3ef1c
Chun <3553680396@qq.com>
2026-05-04 17:09:04 +0800

    feat:论文初稿和第一版检测报告

---
+++ docs/AIGC检测报告/js_v2/report.js
@@ -0,0 +1,4 @@
+var reportLock = true;
+var reportSimilarity = "2.1%";
+var reportUrl = "https://report.paperyy.com/20260503/7-174cfa9c-92f9-4230-a0c9-ac0673f78eea/contrast.html?AccessKeyId=HBDT3R50QARZFDNEE0FT&Expires=1778399331&Signature=66myIjWc1ABz3F4jtMDP/wWY/HY%3D";
+var zipReportUrl = "https://report.paperyy.com/20260503/7-a66032fe9d6142f7a75636c0854ad45c-aigc/report.zip?AccessKeyId=HBDT3R50QARZFDNEE0FT&Expires=1778399331&Signature=NeGGNuczj8M7sfnks78vufa98uo%3D";
